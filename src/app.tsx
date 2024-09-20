dc543c1c3596eb102ece19065c15d662c6d4166d
Wojciech Malarski <w.malarski@gmail.com>
2024-09-20 18:21:37 +0200

    adds create todo form

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import "./app.css";
+import { CreateTodoForm } from "./components/todos/CreateTodoForm";
 import { TodosList } from "./components/todos/TodosList";
 import { TodosStoreProvider } from "./store/context";
 import { INITIAL_STORE_STATE } from "./store/create";
@@ -7,6 +8,7 @@ export default function App() {
 	return (
 		<TodosStoreProvider initialTodos={INITIAL_STORE_STATE}>
 			<main>
+				<CreateTodoForm />
 				<TodosList />
 			</main>
 		</TodosStoreProvider>
---
+++ src/components/todos/CreateTodoForm.tsx
@@ -0,0 +1,28 @@
+import type { Component, ComponentProps } from "solid-js";
+import { createTodo } from "~/store/actions";
+import { useTodosStore } from "~/store/context";
+import { Button } from "../ui/button";
+import { Field } from "../ui/field";
+
+export const CreateTodoForm: Component = () => {
+	const todosStore = useTodosStore();
+
+	const onSubmit: ComponentProps<"form">["onSubmit"] = (event) => {
+		event.preventDefault();
+
+		const formData = new FormData(event.currentTarget);
+		const text = formData.get("text") as string;
+
+		createTodo(todosStore(), text);
+	};
+
+	return (
+		<form onSubmit={onSubmit}>
+			<Field.Root>
+				<Field.Label>Text</Field.Label>
+				<Field.Input type="text" name="text" placeholder="Text" />
+			</Field.Root>
+			<Button type="submit">Submit</Button>
+		</form>
+	);
+};
