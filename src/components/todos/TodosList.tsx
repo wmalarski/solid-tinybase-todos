38cedbac04a9cdb59331a3d24f2085d001b12a73
Wojciech Malarski <w.malarski@gmail.com>
2024-09-20 18:33:08 +0200

    working subscription to tinybase

--- src/components/todos/TodosList.tsx
+++ src/components/todos/TodosList.tsx
@@ -1,9 +1,28 @@
-import type { Component } from "solid-js";
+import { type Component, createMemo, onCleanup } from "solid-js";
+import { createStore, reconcile } from "solid-js/store";
 import { selectTodos } from "~/store/actions";
 import { useTodosStore } from "~/store/context";
+import { TODOS_TABLE_ID } from "~/store/create";
 
 export const TodosList: Component = () => {
 	const todosStore = useTodosStore();
 
-	return <pre>{JSON.stringify(selectTodos(todosStore()), null, 2)}</pre>;
+	const todos = createMemo(() => {
+		const store = todosStore();
+		const initialValue = selectTodos(store);
+		const [todos, setTodos] = createStore(initialValue);
+
+		const listenerId = store.addRowIdsListener(TODOS_TABLE_ID, (store) => {
+			const updatedValue = selectTodos(store);
+			setTodos(reconcile(updatedValue));
+		});
+
+		onCleanup(() => {
+			store.delListener(listenerId);
+		});
+
+		return todos;
+	});
+
+	return <pre>{JSON.stringify(todos(), null, 2)}</pre>;
 };
